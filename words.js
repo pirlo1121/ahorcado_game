b6a0e8d5f13d9895546007c9827b409588002d46
Maicol <pirlo1121s@gmail.com>
2026-02-05 10:37:20 -0500

    delete .gitignore

---
+++ words.js
@@ -0,0 +1,37 @@
+window.gameData = {
+    "Animales": [
+        "elefante", "jirafa", "hipopotamo", "rinoceronte", "cocodrilo", "murcielago", "canguro", "chimpance", "gorila", "avestruz",
+        "pinguino", "ornitorrinco", "armadillo", "camaleon", "salamandra", "escorpion", "tarantula", "aguila", "halcon", "buitre",
+        "pelicano", "flamenco", "guacamayo", "colibri", "leopardo", "guepardo", "jaguar", "pantera", "tigre", "leon",
+        "lobo", "hiena", "zorro", "mapache", "castor", "nutria", "delfin", "ballena", "orca", "cachalote",
+        "pulpo", "calamar", "medusa", "langosta", "escarabajo", "mariposa", "libelula", "saltamontes", "hormiga", "abeja"
+    ],
+    "Paises": [
+        "argentina", "brasil", "colombia", "mexico", "estadosunidos", "canada", "alemania", "francia", "italia", "espana",
+        "portugal", "inglaterra", "rusia", "china", "japon", "corea", "india", "australia", "nuevacelanda", "egipto",
+        "sudafrica", "marruecos", "nigeria", "kenia", "turquia", "grecia", "suecia", "noruega", "finlandia", "dinamarca",
+        "polonia", "ucrania", "austria", "suiza", "belgica", "holanda", "irlanda", "escocia", "gales", "chile",
+        "peru", "ecuador", "venezuela", "uruguay", "paraguay", "bolivia", "cuba", "jamaica", "haiti", "panama"
+    ],
+    "Frutas": [
+        "manzana", "platano", "naranja", "uva", "fresa", "sandia", "melon", "piña", "mango", "papaya",
+        "kiwi", "limon", "lima", "mandarina", "toronja", "cereza", "ciruela", "durazno", "albaricoque", "higo",
+        "datil", "coco", "aguacate", "tomate", "pepino", "calabaza", "berenjena", "pimiento", "arandano", "frambuesa",
+        "mora", "grosella", "granada", "chirimoya", "guanabana", "maracuya", "guayaba", "litchi", "caqui", "membrillo",
+        "nispero", "tamarindo", "carambola", "pitahaya", "rambutan", "mangostan", "zapote", "borojo", "uvas", "limones"
+    ],
+    "Deportes": [
+        "futbol", "baloncesto", "voleibol", "tenis", "beisbol", "golf", "natacion", "atletismo", "ciclismo", "boxeo",
+        "judo", "karate", "taekwondo", "lucha", "esgrima", "gimnasia", "hockey", "rugby", "cricket", "badminton",
+        "pingpong", "surf", "skate", "snowboard", "esqui", "patinaje", "remo", "piraguismo", "vela", "buceo",
+        "escalada", "alpinismo", "senderismo", "paracaidismo", "parapente", "automovilismo", "motociclismo", "triatlon", "pentatlon", "decatlon",
+        "balonmano", "waterpolo", "softbol", "squash", "racquetball", "billar", "bolos", "dardos", "ajedrez", "polo"
+    ],
+    "Profesiones": [
+        "medico", "enfermero", "abogado", "ingeniero", "arquitecto", "maestro", "profesor", "cientifico", "biologo", "quimico",
+        "fisico", "matematico", "astronomo", "geologo", "paleontologo", "arqueologo", "historiador", "sociologo", "psicologo", "filosofo",
+        "escritor", "periodista", "actor", "cantante", "musico", "pintor", "escultor", "fotografo", "cineasta", "disenador",
+        "programador", "mecanico", "electricista", "carpintero", "fontanero", "albanil", "soldador", "jardinero", "agricultor", "ganadero",
+        "pescador", "chef", "cocinero", "camarero", "policia", "bombero", "piloto", "azafata", "fiosoterapeuta", "dentista"
+    ]
+};
